5329739abf54d012ecd6ddd1641b258246d27579
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-13 07:27:30 +0300

    Add UsersDto description

--- src/main/java/by/tishalovichm/crudapp/dto/UserDto.java
+++ src/main/java/by/tishalovichm/crudapp/dto/UserDto.java
@@ -1,5 +1,6 @@
 package by.tishalovichm.crudapp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "UserDto information"
+)
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,12 +19,21 @@ public class UserDto {
 
     private Long id;
 
+    @Schema(
+            description = "User first name"
+    )
     @NotEmpty(message = "User's first name shouldn't be null or empty")
     private String firstName;
 
+    @Schema(
+            description = "User last name"
+    )
     @NotEmpty(message = "User's last name shouldn't be null or empty")
     private String lastName;
 
+    @Schema(
+            description = "User email address"
+    )
     @NotEmpty(message = "User's email shouldn't be null or empty")
     @Email(message = "User's email should be formed as valid email")
     private String email;
