5a142c2e29f9fe91926779c8e018f8952e88ef16
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-13 07:20:05 +0300

    Add api and endpoints description

--- src/main/java/by/tishalovichm/crudapp/controller/UserController.java
+++ src/main/java/by/tishalovichm/crudapp/controller/UserController.java
@@ -2,6 +2,10 @@ package by.tishalovichm.crudapp.controller;
 
 import by.tishalovichm.crudapp.dto.UserDto;
 import by.tishalovichm.crudapp.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(
+        name = "CRUD rest API for user entity",
+        description = "This API contains CRUD functionality for users manipulating"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -16,35 +24,95 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+        summary = "Create new user",
+        description = "Used to insert new user to a database"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "User successfully added"
+                    ),
+            }
+    )
     @PostMapping("")
-    public ResponseEntity<UserDto> createUserDto(@Valid @RequestBody UserDto user) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
         UserDto createdUserDto = userService.createUser(user);
 
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Retrieving user by id",
+            description = "Used to get used by id from database"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User successfully found"
+                    ),
+            }
+    )
     @GetMapping("{id}")
-    public ResponseEntity<UserDto> getUserDto(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    @Operation(
+            summary = "Retrieving all users",
+            description = "Used to get all users from database"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "All users are retrieved"
+                    ),
+            }
+    )
     @GetMapping("")
-    public ResponseEntity<List<UserDto>> getAllUserDtos() {
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @Operation(
+            summary = "Update a user",
+            description = "Used to update user with certain id from database"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User successfully updated"
+                    ),
+            }
+    )
     @PutMapping("{id}")
-    public ResponseEntity<UserDto> updateUserDto(
+    public ResponseEntity<UserDto> updateUser(
             @PathVariable Long id, @Valid @RequestBody UserDto user) {
 
         user.setId(id);
         return ResponseEntity.ok(userService.updateUser(user));
     }
 
+    @Operation(
+            summary = "Delete a user",
+            description = "Used to delete a user by id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "User successfully deleted"
+                    ),
+            }
+    )
     @DeleteMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUserDto(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
