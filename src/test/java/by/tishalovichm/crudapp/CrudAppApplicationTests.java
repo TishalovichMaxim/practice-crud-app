84f9691b3b773f51f749935034d29e5d17388bc2
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-18 13:00:19 +0300

    Remove test methods in autogenerated tests to don't cause build errors

--- src/test/java/by/tishalovichm/crudapp/CrudAppApplicationTests.java
+++ src/test/java/by/tishalovichm/crudapp/CrudAppApplicationTests.java
@@ -1,13 +1,7 @@
 package by.tishalovichm.crudapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrudAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
